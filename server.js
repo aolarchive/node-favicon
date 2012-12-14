c414bbafa38c5a4d67980d19f8f6192b7f980598
Dave Artz <dave@artzstudio.com>
2012-12-14 13:45:48 -0500

    Implemented favicon cache expiration, hard coded to 1 day; closes #1.

--- server.js
+++ server.js
@@ -142,12 +142,12 @@ http.createServer(function (request, response) {
     // These variables help us know when both
     // requests have returned and we can complete
     // the request.
-    promiseReturned = 0,
-    promiseExpected = 2,
+    returned = 0,
+    expected = 2,
     done = function () {
-      promiseReturned += 1;
+      returned += 1;
       var favicon;
-      if (promiseReturned === promiseExpected) {
+      if (returned === expected) {
         // If we have an html favicon, let's use that.
         if (htmlFavicon) {
           favicon = htmlFavicon;
@@ -164,6 +164,7 @@ http.createServer(function (request, response) {
       }
     };
 
+
   if (!/http[s]*:\/\//.test(root)) {
     root = 'http://' + root;
   }
@@ -173,8 +174,17 @@ http.createServer(function (request, response) {
 
   // See if we have the favicon in our cache.
   fs.stat(__dirname + '/favicons/' + host + '.ico', function (err, stats) {
-    // If there's an error, we don't have it.
-    if (err) {
+
+    // If we have stats on the file, see if we need to refresh if
+    // greater than our time limit.
+    if (stats) {
+      var expires = 24 * 60 * 60 * 1000, // One day in milliseconds.
+        isExpired = new Date() - stats.mtime > expires;
+    }
+
+    // If there's an error, or the file has expired,
+    // we need to fetch the favicon.
+    if (err || isExpired) {
       // Try fetching the icon from the root of the domain.
       getFavicon(root + '/favicon.ico', function (favicon) {
         // If we got one, save it to disk and return it.
@@ -207,6 +217,7 @@ http.createServer(function (request, response) {
         }
       });
     } else {
+      // console.log(host + '.ico file stats: ', stats);
       fs.readFile(__dirname + '/favicons/' + host + '.ico', function (err, favicon) {
         if (!err) {
           response.writeHead(200, {'Content-Type': 'image/x-icon'});
